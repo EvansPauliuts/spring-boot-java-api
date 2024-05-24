015a7692485cebc1febee95a017bdb70e61bff9d
Evans <pauliutsevans@gmail.com>
2024-05-24 09:51:32 +0300

    Day 1 - 1

---
+++ src/main/java/dev/evansdev/SpringBootJavaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.evansdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavaApiApplication.class, args);
+	}
+
+}
